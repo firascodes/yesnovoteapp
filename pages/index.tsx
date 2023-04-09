8b70983eccba2c6a585d56485837243edb613dd7
Firas <firasmumtaz12@gmail.com>
2023-04-09 17:50:16 +0530

    Integrated contract and added functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,153 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useContractRead, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
+
 const Home: NextPage = () => {
+
+  const address= useAddress();
+  const contractAddress = "0x1cA2353648b3D239CDb205D4eEe86438045B6d31";
+
+  const { contract } = useContract(contractAddress);
+  const { data: proposal1, isLoading: proposal1Loading } = useContractRead(contract,"proposals",0);
+  const { data: proposal2, isLoading: proposal2Loading } = useContractRead(contract,"proposals",3);
+  const { data: hasVoted, isLoading: hasVotedLoading } = useContractRead(contract, "hasVoted", address);
+  
+  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+      <h1>Decentralized Opinion/Survey Board</h1>
+      <p><i>(One vote per Account)</i></p>
+      <br />
+      <ConnectWallet theme="dark"/>
+      <br />
+      <div className={styles.cardContainer}>
+          <div className={styles.card}>
+            <h1>Question #1</h1>
+            <div>
+            {address ? (
+              <div>
+                { proposal1Loading ? (
+                  <div>
+                    <p> Loading Proposal ....</p>
+                  </div>
+                ): (
+                  <div>
+                    <h2>{proposal1[0]}</h2>
+                    {
+                      !hasVoted?<div className={styles.btns}>
+                      <Web3Button className={styles.web3btn}
+                      contractAddress={contractAddress}
+                      action={(contract)=> contract.call("voteYes",0)}
+                      onSuccess={(result) => alert("Success!")}
+                      onSubmit={() => console.log("Transaction submitted")}
+                      theme="light"
+                      isDisabled={hasVoted}
+                      >Yes</Web3Button>
+                      <br />
+                      <br />
+                      <Web3Button className={styles.web3btn}
+                      contractAddress={contractAddress}
+                      action={(contract)=> contract.call("voteNo",0)}
+                      onSuccess={(result) => alert("Success!")}
+                      onSubmit={() => console.log("Transaction submitted")}
+                      theme="light"
+                      isDisabled={hasVoted}
+                      >No</Web3Button>
+                    </div> :
+                      <h4>You Have Voted Successfully!</h4>
+                    }
+                    <div>
+                      {!hasVotedLoading && hasVoted? (
+                        <div>
+                            <h3>Result: </h3>
+                            <p>Yes:  {proposal1[1].toNumber()}</p>
+                            <p>No:  {proposal1[2].toNumber()}</p>
+                        </div>
+                      ): (
+                        <div>
+                          <br />
+                          <p>You have not voted yet. Result will show after you vote</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+              ) : (
+                <p>Please connect your wallet to continue.</p>
+              )}
+            </div>
+          </div>
+          {/* <div className={styles.or}>
+            <h1>Or</h1>
+          </div> */}
+          <div className={styles.card}>
+            <h1>Question #2</h1>
+            <div>
+            {address ? (
+              <div>
+                { proposal2Loading ? (
+                  <div>
+                    <p> Loading Proposal ....</p>
+                  </div>
+                ): (
+                  <div>
+                    <h2>{proposal2[0]}</h2>
+                    { !hasVoted? <div className={styles.btns}>
+                      <Web3Button className={styles.web3btn}
+                      contractAddress={contractAddress}
+                      action={(contract)=> contract.call("voteYes",3)}
+                      onSuccess={(result) => alert("Success!")}
+                      onSubmit={() => console.log("Transaction submitted")}
+                      theme="light"
+                      isDisabled={hasVoted}
+                      >Yes</Web3Button>
+                      <br />
+                      <br />
+                      <Web3Button className={styles.web3btn}
+                      contractAddress={contractAddress}
+                      action={(contract)=> contract.call("voteNo",3)}
+                      onSuccess={(result) => alert("Success!")}
+                      onSubmit={() => console.log("Transaction submitted")}
+                      theme="light"
+                      isDisabled={hasVoted}
+                      
+                      >No</Web3Button>                  
+                    </div> : <h4>You Have Voted Successfully!</h4>
+                    }
+                    <div>
+                      {!hasVotedLoading && hasVoted? (
+                        <div>
+                            <h3>Result: </h3>
+                            <p>Yes:  {proposal2[1].toNumber()}</p>
+                            <p>No:  {proposal2[2].toNumber()}</p>
+                        </div>
+                      ): (
+                        <div>
+                          <br />
+                          <p>You have not voted yet. Result will show after you vote</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+              ) : (
+                <p>Please connect your wallet to continue.</p>
+              )}
+            </div>
+          </div>
+      </div>
       </main>
     </div>
   );
+
+  
+
+
 };
 
 export default Home;
